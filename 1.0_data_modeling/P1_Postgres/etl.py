ba40c1979bf41963be707c90d18fdb7f27ac99d1
jpbacher <bacherjp@gmail.com>
2020-11-29 20:01:38 -0500

    making functions

--- 1.0_data_modeling/P1_Postgres/etl.py
+++ 1.0_data_modeling/P1_Postgres/etl.py
@@ -6,55 +6,51 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    # open song file
-    df = 
+    # song file
+    song_df = pd.read_json(filepath, lines=True)
+    for value in song_df.values:
+        num_songs, artist_id, artist_latitude, artist_longitude, artist_location, artist_name, song_id, title, duration, year = value
+        # insert artist record
+        artist_data = [artist_id, artist_name, artist_location, artist_latitude, artist_longitude]
+        cur.execute(artist_table_insert, artist_data)
 
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
-    
-    # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+        # insert song record
+        song_data = [song_id, title, artist_id, year, duration]
+        cur.execute(song_table_insert, song_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
-
+    log_df = pd.read_json(filepath, lines=True)
     # filter by NextSong action
-    df = 
-
+    log_df = log_df.loc[log_df['page'] == 'NextSong']
     # convert timestamp column to datetime
-    t = 
-    
+    t_col = pd.to_datetime(log_df['ts'], unit='ms')
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = []
+    for row in t_col:
+        time_data.append([row, row.hour, row.day, row.week, row.month, row.year, row.day_name()])
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame.to_records(time_data, columns=column_labels)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
-
+    user_df = log_df[['userId', 'firstName', 'lastName', 'gender', 'level']]
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
     # insert songplay records
-    for index, row in df.iterrows():
-        
+    for index, row in log_df.iterrows():
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
-
         # insert songplay record
         songplay_data = 
         cur.execute(songplay_table_insert, songplay_data)
@@ -70,17 +66,17 @@ def process_data(cur, conn, filepath, func):
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print(f'{num_files} files found in {filepath}')
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+        print(f'{i}/{num_files} files processed.')
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=joshbacher")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
