1eaff09ba0c4dee84ce85891b731fd6ffe8e323f
jpbacher <bacherjp@gmail.com>
2020-12-17 17:32:23 -0500

    added song inset

--- 2.0_dwh/sql_queries.py
+++ 2.0_dwh/sql_queries.py
@@ -156,6 +156,14 @@ user_table_insert = ("""
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    SELECT DISTINCT(song_id) AS song_id,
+           title,
+           artist_id,
+           year, 
+           duration
+    FROM staging_songs
+    WHERE song_id IS NOT NULL;
 """)
 
 artist_table_insert = ("""
