ddfd84008b4fcf14f3c7721757a8beeb9d03cd8e
jpbacher <bacherjp@gmail.com>
2020-12-28 14:39:29 -0500

    added print statements

--- 2.0_dwh/create_tables.py
+++ 2.0_dwh/create_tables.py
@@ -4,13 +4,17 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    # delete any pre-existing tables
+    print('Dropping tables...')
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    # create staging and dimensional tables
     for query in create_table_queries:
+        print(f'Running {query}...')
         cur.execute(query)
         conn.commit()
 
@@ -21,6 +25,7 @@ def main():
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
+    print('Connected to cluster...')
 
     drop_tables(cur, conn)
     create_tables(cur, conn)
