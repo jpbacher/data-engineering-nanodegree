d24fa69fa7aee7c61863d9a41e399688ec8d47a7
jpbacher <bacherjp@gmail.com>
2020-12-28 18:36:08 -0500

    added open_port function

--- 2.0_dwh/build_cluster.py
+++ 2.0_dwh/build_cluster.py
@@ -6,13 +6,13 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-def make_iam_role(iam, DWH_IAM_ROLE_NAME):
+def make_iam_role(iam, iam_role_name):
     # make an IAM role for Redshift
     try:
         print('Creating new IAM Role...')
         dwh_role = iam.create_role(
             Path='/',
-            RoleName=DWH_IAM_ROLE_NAME,
+            RoleName=iam_role_name,
             Description='Permits Redshift cluster to call AWS services',
             AssumeRolePolicyDocument=json.dumps(
                 {'Statement': [{'Action': 'sts.AssumeRole',
@@ -26,12 +26,12 @@ def make_iam_role(iam, DWH_IAM_ROLE_NAME):
 
     print('Attaching policy...')
     iam.attach_role_policy(
-        RoleName=DWH_IAM_ROLE_NAME,
+        RoleName=iam_role_name,
         PolicyArn='arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'
     )['ResponseMetadata']['HTTPStatusCode']
 
     print('Get IAM Role ARN...')
-    role_arn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
+    role_arn = iam.get_role(RoleName=iam_role_name)['Role']['Arn']
     print(f'role_arn: {role_arn}')
     return role_arn
 
@@ -62,5 +62,22 @@ def get_cluster_properties(redshift, cluster_identifier):
     print(f'DWH_ENDPOINT: {dwh_endpoint}')
     print(f'DWH_ROLE_ARN: {dwh_role_arn}')
     return cluster_props, dwh_endpoint, dwh_role_arn
+
+
+def open_ports(ec2, cluster_props, dwh_port):
+    # update security group to allow access
+    try:
+        vpc = ec2.Vpc(id=cluster_props['VpcId'])
+        default_sec_grps = list(vpc.security_groups.all())[0]
+        print(f'Security groups: {default_sec_grps}')
+        default_sec_grps.authorize_ingress(
+            GroupName=default_sec_grps.group_name,
+            CirIp='0.0.0.0/0',
+            IpProtocol='TCP',
+            FromPort=int(dwh_port),
+            ToPort=int(dwh_port)
+        )
+    except Exception as e:
+        print(e)
 
 
