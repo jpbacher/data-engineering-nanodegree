2dd85fd2f7078bf1f14f6841f4667d6fb0232f20
jpbacher <bacherjp@gmail.com>
2020-12-31 15:54:28 -0500

    added variables in main function

--- 2.0_dwh/build_cluster.py
+++ 2.0_dwh/build_cluster.py
@@ -10,6 +10,7 @@ def make_iam_role(iam, iam_role_name):
     # make an IAM role to grant permission to Redshift cluster
     try:
         print('Creating new IAM Role...')
+        # must create IAM role in console
         dwh_role = iam.create_role(
             Path='/',
             RoleName=iam_role_name,
@@ -61,7 +62,7 @@ def get_cluster_properties(redshift, dwh_cluster_identifier):
     # get Redshift cluster properties
     def display_redshift_props(properties):
         # display cluster properties, check status is 'Available'
-        pd.set_option('display.max_colwidth', -1)
+        pd.set_option('display.max_colwidth', None)
         prop_keys = ['ClusterIdentifier', 'NodeType', 'ClusterStatus', 'MasterUserName', 'DBName',
                      'Endpoint', 'VpcId', 'NumberOfNodes']
         data = [(key, value) for key, value in properties.items() if key in prop_keys]
@@ -73,7 +74,7 @@ def get_cluster_properties(redshift, dwh_cluster_identifier):
 
     dwh_endpoint = cluster_props['Endpoint']['Address']
     dwh_role_arn = cluster_props['IamRoles'][0]['IamRoleArn']
-    print(f'DWH_ENDPOINT:: {dwh_endpoint}')
+    print(f'DWH_ENDPOINT:: {dwh_endpoint}')  # used for host
     print(f'DWH_ROLE_ARN:: {dwh_role_arn}')
     return cluster_props, dwh_endpoint, dwh_role_arn
 
@@ -115,6 +116,11 @@ def main():
 
     DWH_IAM_ROLE_NAME = config.get('DWH', 'DWH_IAM_ROLE_NAME')
 
+    DB_NAME = config.get('CLUSTER', 'DB_NAME')
+    DB_USER = config.get('CLUSTER', 'DB_USER')
+    DB_PASSWORD = config.get('CLUSTER', 'DB_PASSWORD')
+    DB_PORT = config.get('CLUSTER', 'DB_PORT')
+
     cluster_df = pd.DataFrame(
         dict(Param=['DWH_CLUSTER_TYPE', 'DWH_NUM_NODES', 'DWH_NODE_TYPE',
                     'DWH_CLUSTER_IDENTIFIER', 'DWH_DB', 'DWH_DB_USER',
@@ -148,11 +154,11 @@ def main():
     build_cluster(redshift, role_arn, DWH_CLUSTER_TYPE, DWH_NODE_TYPE, DWH_NUM_NODES,
                   DWH_DB, DWH_CLUSTER_IDENTIFIER, DWH_DB_USER, DWH_DB_PASSWORD)
 
-    cluster_properties = get_cluster_properties(redshift, DWH_CLUSTER_IDENTIFIER)
+    cluster_properties, dwh_endpoint = get_cluster_properties(redshift, DWH_CLUSTER_IDENTIFIER)
     open_ports(ec2, cluster_properties, DWH_PORT)
 
     conn = psycopg2.connect('host={} dbname={} user={} password={} port={}'.format(
-        *config['CLUSTER'].values())
+        dwh_endpoint, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT)
     )
     cur = conn.cursor()
     print('Connected...')
