c1ad0f94d4fa4533bdc7c21c7b39459406b83b5f
jpbacher <bacherjp@gmail.com>
2021-01-21 13:48:20 -0500

    added staging_events table

--- 4.0_data_pipeline/create_tables.sql
+++ 4.0_data_pipeline/create_tables.sql
@@ -27,3 +27,24 @@ CREATE TABLE IF NOT EXISTS public.songs (
     duration numeric(18,0),
     CONSTRAINT song_primkey PRIMARY KEY (songid)
 );
+
+CREATE TABLE IF NOT EXISTS public.staging_events (
+    artist varchar(256),
+    auth varchar(256),
+    firstname varchar(256),
+    gender varchar(256),
+    iteminsession int4,
+    lastname varchar(256),
+    length numeric(18,0),
+    'level' varchar(256),
+    location varchar(256),
+    'method' varchar(256),
+    page varchar(256),
+    registration numeric(18,0),
+    sessionid int4,
+    song varchar(256),
+    status int4,
+    ts int8,
+    useragent varchar(256),
+    userid int4
+);
