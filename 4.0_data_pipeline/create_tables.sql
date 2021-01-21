4ab954859b5a5c53d791ba27a4c9deba8f0b03c7
jpbacher <bacherjp@gmail.com>
2021-01-21 12:51:33 -0500

    added songplays table

--- 4.0_data_pipeline/create_tables.sql
+++ 4.0_data_pipeline/create_tables.sql
@@ -5,3 +5,16 @@ CREATE TABLE IF NOT EXISTS public.artists (
     latitude numeric(18,0),
     longitude numeric(18,0)
 );
+
+CREATE TABLE IF NOT EXISTS public.songplays (
+    playid varchar(32) NOT NULL,
+    start_time timestamp NOT NULL,
+    userid int4 NOT NULL,
+    'level' varchar(256),
+    songid varchar(256),
+    artistid varchar(256),
+    sessionid int4,
+    location varchar(256),
+    user_agent varchar(256),
+    CONSTRAINT songplays_pkey PRIMARY KEY (playid)
+);
