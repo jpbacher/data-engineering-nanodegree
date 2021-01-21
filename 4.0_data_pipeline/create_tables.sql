6c33a49646c35b340c9541a43ffe6938e642fe7d
jpbacher <bacherjp@gmail.com>
2021-01-21 14:14:59 -0500

    added time table

--- 4.0_data_pipeline/create_tables.sql
+++ 4.0_data_pipeline/create_tables.sql
@@ -70,3 +70,14 @@ CREATE TABLE IF NOT EXISTS public.users (
     'level' varchar(256),
     CONSTRAINT user_primkey PRIMARY KEY (userid)
 );
+
+CREATE TABLE IF NOT EXISTS public.'time' (
+    start_time timestamp NOT NULL,
+    'hour' int4,
+    'day' int4,
+    week int4,
+    'month' varchar(256),
+    'year' int4,
+    weekday varchar(256),
+    CONSTRAINT time_primkey PRIMARY KEY (start_time)
+);
