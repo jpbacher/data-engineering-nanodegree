8a68c2d2e297388ac0a7ebf9f4113a2fe21aa629
jpbacher <bacherjp@gmail.com>
2021-01-22 11:01:51 -0500

    added args

--- 4.0_data_pipeline/plugins/operators/stage_redshift.py
+++ 4.0_data_pipeline/plugins/operators/stage_redshift.py
@@ -8,10 +8,28 @@ class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
-    def __init__(self, *args, **kwargs):
+    def __init__(self,
+                 redshift_conn_id='',
+                 aws_credentials_id='',
+                 table='',
+                 s3_bucket='',
+                 s3_key='',
+                 json_path='',
+                 file_format='JSON',
+                 delimiter=',',
+                 ignore_headers=1,
+                 *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_path = json_path
+        self.file_format = file_format
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
