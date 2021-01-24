ed4160f60ad0bdef614b5eb33f20eca76d7d9f1c
jpbacher <bacherjp@gmail.com>
2021-01-24 11:34:01 -0500

    completed DataQualityOperator class

--- 4.0_data_pipeline/plugins/operators/data_quality.py
+++ 4.0_data_pipeline/plugins/operators/data_quality.py
@@ -18,4 +18,17 @@ class DataQualityOperator(BaseOperator):
         self.tables = tables
 
     def execute(self, context):
-        self.log.info('')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+
+        for table in self.tables:
+            self.log.info(f'Inspecting record count of {table} table')
+            cmd = f'SELECT COUNT(*) FROM {table}'
+            records = redshift_hook.get_records(sql=cmd)
+            if len(records[0] < 1) or len(records) < 1:
+                self.log.error(f'Data quality check failed...{table} did not return any results')
+                raise ValueError(f'Data quality check failed...{table} did not return any results')
+            record_count = records[0][0]
+            if record_count < 1:
+                self.log.error(f'Data quality check failed...{table} has 0 rows')
+                raise ValueError(f'Data quality check failed...{table} has 0 rows')
+            self.log.info(f'Data quality check passed - {table} table: {record_count} records')
