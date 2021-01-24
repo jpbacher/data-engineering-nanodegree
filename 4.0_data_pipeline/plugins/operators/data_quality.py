26971ea735d31e742f2d5e7cfa7ca4adaa4136a3
jpbacher <bacherjp@gmail.com>
2021-01-24 10:50:23 -0500

    initialized DataQualityOperator

---
+++ 4.0_data_pipeline/plugins/operators/data_quality.py
@@ -0,0 +1,21 @@
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id='',
+                 tables=[],
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
+
+    def execute(self, context):
+        self.log.info('')
